9c669beea2a88f87ed54a4be5e5978ced4f409e0
Ameet Patankar <ameet1@ameets-mac-pro.lan>
2017-11-21 12:58:19 -0800

    first check in

---
+++ src/TreeComponent/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TreeComponent = ({name , name2} ) => (
+ <div>
+    <div>Hi {name}</div>
+    <div>Hi {name2}</div>
+  </div>  
+);
+
+export default TreeComponent;
